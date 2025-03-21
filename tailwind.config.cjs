0040a6097c4b0e843369699f0b25dfb527cb27a6
John Pierre Mendoza <johnpierremendoza@gmail.com>
2025-03-21 17:12:10 -0400

    refactor: change tab width

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,16 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-	    fontFamily: {
-	        "main": ["JetBrains Mono", "monospace"],
-	    },
+    fontFamily: {
+      "main": ["JetBrains Mono", "monospace"],
+    },
 		extend: {
-	        colors: {
-	            "dark": "#010713",
-	            "dark2": "#10141D",
-	            "light": "#FFFFFF",
-	            "light2": "#E4FDFF"
-	        },
+      colors: {
+        "dark": "#010713",
+        "dark2": "#10141D",
+        "light": "#FFFFFF",
+        "light2": "#E4FDFF"
+      },
 		},
 	},
 	darkMode: "class",
